c8b3154203ef334e668c957ca6d628736cdcd42b
24HP5072 24HP5072 <24HP5072@admins-iMac.local>
2025-01-16 15:40:48 +0800

    cors error fix

--- server.js
+++ server.js
@@ -1,39 +1,36 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const userRoute = require('./Routes/userRoute.js')
-const postRoute = require('./Routes/postRoute.js')
-const commentRoute = require('./Routes/commentRoute.js')
-const likeRoute = require('./Routes/likeRoute.js')
-const cors = require("cors")
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./Routes/userRoute.js");
+const postRoute = require("./Routes/postRoute.js");
+const commentRoute = require("./Routes/commentRoute.js");
+const likeRoute = require("./Routes/likeRoute.js");
+const cors = require("cors");
 
+dotenv.config();
 
-dotenv.config()
+const app = express();
+const port = 8080;
 
-const app = express()
-const port = 8080
-app.use(cors())
-const connectDatabase = async() => {
-    const res = await mongoose.connect(process.env.MONGODB_URI)
+const connectDatabase = async () => {
+  const res = await mongoose.connect(process.env.MONGODB_URI);
 
-    if (res) console.log("db connected")
-    console.log(typeof(process.env.MONGODB_URI))
-}
+  if (res) console.log("db connected");
+  console.log(typeof process.env.MONGODB_URI);
+};
 
-connectDatabase()
-
-app.use(express.json())
+connectDatabase();
+app.use(cors());
+app.use(express.json());
 
 app.use(userRoute);
 
-app.use(postRoute)
+app.use(postRoute);
 
-app.use(commentRoute)
+app.use(commentRoute);
 
-app.use(likeRoute)
-
-
+app.use(likeRoute);
 
 app.listen(port, () => {
-    console.log(`your backend server is running on ${port}`)
-})
+  console.log(`your backend server is running on ${port}`);
+});
